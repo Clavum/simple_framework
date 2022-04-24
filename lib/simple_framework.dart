02c8e5ad370cb4e513cc89249d33c65d527f0b7d
Joseph <joseph@asdc.net>
2022-04-24 16:18:41 -0400

    Move files to src so they are hidden from importing

--- lib/simple_framework.dart
+++ lib/simple_framework.dart
@@ -1,7 +1,14 @@
 library simple_framework;
 
-export 'package:simple_framework/core/core.dart';
+export 'package:simple_framework/src/core/bloc.dart';
+export 'package:simple_framework/src/core/entity.dart';
+export 'package:simple_framework/src/core/locator.dart';
+export 'package:simple_framework/src/core/mock_class_provider.dart';
+export 'package:simple_framework/src/core/repository.dart';
+export 'package:simple_framework/src/core/screen.dart';
+export 'package:simple_framework/src/core/use_case.dart';
 
-export 'package:simple_framework/test_helpers/test_helpers.dart';
+export 'package:simple_framework/src/test_helpers/bloc_test_helpers.dart';
+export 'package:simple_framework/src/test_helpers/general_helpers.dart';
 
 export 'package:entity_generator/entity_generator.dart';
--- lib/test_helpers/test_helpers.dart
+++
@@ -1,4 +0,0 @@
-library test_helpers;
-
-export 'package:simple_framework/test_helpers/src/bloc_test_helpers.dart';
-export 'package:simple_framework/test_helpers/src/general_helpers.dart';
