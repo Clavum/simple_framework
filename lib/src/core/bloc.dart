cab6e3ed2675f05080995cced3be16694adec96f
Joseph <joseph@asdc.net>
2022-07-21 18:25:58 -0400

    Use Bloc for building ViewModels

--- lib/src/core/bloc.dart
+++ lib/src/core/bloc.dart
@@ -1,11 +1,16 @@
-abstract class Bloc {
+import 'dart:async';
+
+import 'package:simple_framework/src/core/models/view_model.dart';
+import 'package:simple_framework/src/core/ui/screen_ref.dart';
+
+abstract class Bloc<V extends ViewModel> {
   /// Optional method to override, to trigger events when the Screen (and Bloc) are first created.
   /// The return type is `Future<bool?>` because the Screen will show the loading screen until the
   /// Future is complete. You can also optionally return `false` to show the error screen.
   Future<void> onCreate() async {}
 
+  FutureOr<V> buildViewModel(ScreenRef ref);
+
   /// Optional method to override, to trigger events when the Screen (and Bloc) are disposed.
   void dispose() {}
 }
-
-class EmptyBloc extends Bloc {}
--- lib/src/core/ui/screen.dart
+++ lib/src/core/ui/screen.dart
@@ -4,12 +4,10 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_framework/simple_framework.dart';
 
-abstract class Screen<B extends Bloc, V extends ViewModel> extends StatefulWidget {
+abstract class Screen<B extends Bloc<V>, V extends ViewModel> extends StatefulWidget {
   final B _bloc;
 
-  final ModelBuilder<V> _builder;
-
-  const Screen(this._bloc, this._builder, {Key? key}) : super(key: key);
+  const Screen(this._bloc, {Key? key}) : super(key: key);
 
   Widget build(BuildContext context, B bloc, V viewModel);
 
@@ -26,7 +24,7 @@ abstract class Screen<B extends Bloc, V extends ViewModel> extends StatefulWidge
   _ScreenState<B, V> createState() => _ScreenState<B, V>();
 }
 
-class _ScreenState<B extends Bloc, V extends ViewModel> extends State<Screen<B, V>> {
+class _ScreenState<B extends Bloc<V>, V extends ViewModel> extends State<Screen<B, V>> {
   late ScreenRef _ref;
 
   V? _viewModel;
@@ -46,7 +44,7 @@ class _ScreenState<B extends Bloc, V extends ViewModel> extends State<Screen<B, 
   /// A loading screen will be shown until the builder is done loading.
   void awaitBuilder() async {
     await widget._bloc.onCreate();
-    _viewModel = await widget._builder.build(_ref);
+    _viewModel = await widget._bloc.buildViewModel(_ref);
     _ref.firstLoad = false;
     setState(() {
       loading = false;
@@ -76,8 +74,8 @@ class _ScreenState<B extends Bloc, V extends ViewModel> extends State<Screen<B, 
 
   void setUpRef() {
     _ref = ScreenRef(<T extends RepositoryModel>() {
-      _streams[T] ??= Repository().streamOf<T>().listen((entity) async {
-        var nextViewModel = await widget._builder.build(_ref);
+      _streams[T] ??= Repository().streamOf<T>().listen((_) async {
+        var nextViewModel = await widget._bloc.buildViewModel(_ref);
         if (nextViewModel != _viewModel) {
           setState(() {
             _viewModel = nextViewModel;
