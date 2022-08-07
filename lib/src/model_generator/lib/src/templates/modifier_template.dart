fb9ead5d7a4c3f43f24a0b7ca724ca5150793323
Joseph <joseph@asdc.net>
2022-08-07 15:11:22 -0400

    Remove more TODOs

--- lib/src/model_generator/lib/src/templates/modifier_template.dart
+++ lib/src/model_generator/lib/src/templates/modifier_template.dart
@@ -1,11 +1,5 @@
 import 'package:model_generator/src/model.dart';
 
-//TODO: Add a merge method for quickly setting multiple fields at once.
-//TODO: Right now, I provide a get() method to get the current Entity, so that you can call
-//custom methods on it. Ideally, you should be able to call methods directly on the Modifier, but
-//this would involve visiting every method and it's parameters, then generating:
-//ReturnType methodName(ParameterType parameterName) => _model.methodName(parameterName);
-//But then what about if the Model mixins a class? Not allowed, I guess...
 class ModifierTemplate {
   final Model model;
 
