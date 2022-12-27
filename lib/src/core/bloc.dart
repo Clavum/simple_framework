71f7496a509f557482d8f474ebdcf332583f92df
Joseph <joseph@asdc.net>
2022-12-26 20:45:19 -0500

    Allow mocking bloc context

--- lib/src/core/bloc.dart
+++ lib/src/core/bloc.dart
@@ -3,8 +3,13 @@ part of 'core.dart';
 abstract class Bloc<V extends ViewModel> {
   // Set by the ScreenState.
   late ScreenState _screenState;
+  BuildContext? _mockContext;
 
-  BuildContext get context => _screenState.context;
+  BuildContext get context => _mockContext ?? _screenState.context;
+
+  /// If creating a bloc for unit tests, you'll need to use this to provide a mock context.
+  @visibleForTesting
+  set context(BuildContext value) => _mockContext = value;
 
   /// Optional method to override, to trigger events when the [Bloc] is first created by a [Screen].
   /// If your onCreate method is asynchronous, the [Screen] will display its loading screen until
